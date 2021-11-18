f8581341049be8b41a95c9e25d9fd01e94c43188
MD Rajib <83833885+blackweb71@users.noreply.github.com>
2021-11-18 12:06:34 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+x = document.querySelector("title");
+a = document.querySelector(".but1");
+b = document.querySelector(".but2");
+c = document.querySelector(".but3");
+d = document.querySelector(".but4");
+e = document.querySelector(".but5");
+f = document.querySelector(".but6");
+g = document.querySelector(".but7");
+h = document.querySelector(".but8");
+
+
+x.innerHTML="Villain";
+a.innerHTML="Doctor";
+b.innerHTML="MGR Magan";
+c.innerHTML="Gully Rowdy";
+d.innerHTML="Thinkalazhcha Nishchayam";
+e.innerHTML="Erida";
+f.innerHTML="Cinderella";
+g.innerHTML="Love Story";
+h.innerHTML="Oh Manapenne";
+
+
